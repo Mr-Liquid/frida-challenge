dd717fbe8f1237cbd0db76c88c42575bccda0c61
Ivan <ivan.webgroup@gmail.com>
2016-10-20 15:04:54 +0300

    commit process start

--- frida-test/src/app/app.module.ts
+++ frida-test/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { TabsModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
+    TabsModule,
     BrowserModule,
     FormsModule,
     HttpModule
